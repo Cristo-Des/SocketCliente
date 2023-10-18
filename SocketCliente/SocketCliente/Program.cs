b663d856c099806ca25053e7a023f6eadadf7042
Cristobal Desaily Elizondo Ramos <77365941+Cristo-Des@users.noreply.github.com>
2023-10-18 03:11:14 -0600

    Add files via upload

---
+++ SocketCliente/SocketCliente/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SocketCliente
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //CONFIgurar SERVER
+            IPHostEntry host = Dns.GetHostEntry("localhost");
+            IPAddress ipAddress = host.AddressList[0];
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11200);
+            try
+            {
+                //Creacion de socket
+                Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                //conectar y enviar texto
+                sender.Connect(remoteEP);
+                Console.WriteLine("Conectado Con El Servidor");
+
+                Console.WriteLine("Ingrese Un Texto Para Enviar");
+                string texto = Console.ReadLine();
+                //convercion a byetes
+                byte[] msg = Encoding.ASCII.GetBytes(texto + "<EOF>");
+                //enviar texto
+                int byteSent = sender.Send(msg);
+                sender.Shutdown(SocketShutdown.Both);
+               sender.Close();
+
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
